8ef2f376971760bf03fde538fd0dd4087e31c36c
Asif Hossain <asifh9277@gmail.com>
2023-05-15 20:26:56 +0600

    changes

--- src/Componants/UsersTable/UsersTable.js
+++ src/Componants/UsersTable/UsersTable.js
@@ -4,51 +4,51 @@ import UserSortDropDown from "./UserSortDropDown";
 import UsersTableFooter from "./UsersTableFooter";
 
 const UsersTable = () => {
-
-    const users = [
-      {
-        name: "Devid Warner",
-        email: "David@warner.com",
-        userType: "Trainee",
-        accountCreated: "19 April 2023",
-      },
-      {
-        name: "Devid Warner",
-        email: "David@warner.com",
-        userType: "Trainee",
-        accountCreated: "19 April 2023",
-      },
-      {
-        name: "Devid Warner",
-        email: "David@warner.com",
-        userType: "Trainee",
-        accountCreated: "19 April 2023",
-      },
-      {
-        name: "Devid Warner",
-        email: "David@warner.com",
-        userType: "Trainee",
-        accountCreated: "19 April 2023",
-      }
-    ];
+  const users = [
+    {
+      name: "Devid Warner",
+      email: "David@warner.com",
+      userType: "Trainee",
+      accountCreated: "19 April 2023",
+    },
+    {
+      name: "Devid Warner",
+      email: "David@warner.com",
+      userType: "Trainee",
+      accountCreated: "19 April 2023",
+    },
+    {
+      name: "Devid Warner",
+      email: "David@warner.com",
+      userType: "Trainee",
+      accountCreated: "19 April 2023",
+    },
+    {
+      name: "Devid Warner",
+      email: "David@warner.com",
+      userType: "Trainee",
+      accountCreated: "19 April 2023",
+    },
+  ];
 
   return (
     <div className="p-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
       <div className="flex justify-between">
-
         {/* this is search bar */}
         <div className="w-[70%]">
           <UserSearchBar />
         </div>
-        
+
         {/* this is sorting dropdowns */}
         <div className="flex gap-3 me-5">
           <UserSortDropDown />
           <UserSortDropDown />
         </div>
-
       </div>
-        {/* this is table footer part */}
+      {/* table site */}
+      <div></div>
+
+      {/* this is table footer part */}
       <UsersTableFooter />
     </div>
   );
