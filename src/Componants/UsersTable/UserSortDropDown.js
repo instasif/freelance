8bde1387df709f65a9548bbc021982d5a419b4b9
Asif Hossain <asifh9277@gmail.com>
2023-05-15 19:52:17 +0600

    first commit

---
+++ src/Componants/UsersTable/UserSortDropDown.js
@@ -0,0 +1,78 @@
+import React from "react";
+// flex items-center justify-evenly
+const UserSortDropDown = () => {
+  return (
+    <div>
+      <button
+        id="dropdownDefaultButton"
+        data-dropdown-toggle="dropdown"
+        class="text-black text-sm focus:outline-none rounded-lg font-normal px-14 py-2.5 border"
+        type="button"
+      >
+        <div className="flex justify-between items-center">
+          <div><p>All</p></div>
+          <svg
+            class="w-4 h-4 ml-2"
+            aria-hidden="true"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M19 9l-7 7-7-7"
+            ></path>
+          </svg>
+        </div>
+      </button>
+      {/* <!-- Dropdown menu --> */}
+      <div
+        id="dropdown"
+        class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+      >
+        <ul
+          class="py-2 text-sm text-gray-700 dark:text-gray-200"
+          aria-labelledby="dropdownDefaultButton"
+        >
+          <li>
+            <a
+              href="/"
+              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Dashboard
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Settings
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Earnings
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Sign out
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default UserSortDropDown;
---
+++ src/Componants/UsersTable/UsersTable.js
@@ -0,0 +1,55 @@
+import React from "react";
+import UserSearchBar from "./UserSearchBar";
+import UserSortDropDown from "./UserSortDropDown";
+
+const UsersTable = () => {
+
+    const users = [
+      {
+        name: "Devid Warner",
+        email: "David@warner.com",
+        userType: "Trainee",
+        accountCreated: "19 April 2023",
+      },
+      {
+        name: "Devid Warner",
+        email: "David@warner.com",
+        userType: "Trainee",
+        accountCreated: "19 April 2023",
+      },
+      {
+        name: "Devid Warner",
+        email: "David@warner.com",
+        userType: "Trainee",
+        accountCreated: "19 April 2023",
+      },
+      {
+        name: "Devid Warner",
+        email: "David@warner.com",
+        userType: "Trainee",
+        accountCreated: "19 April 2023",
+      }
+    ];
+
+  return (
+    <div className="p-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
+      <div className="flex justify-between">
+
+        {/* this is search bar */}
+        <div className="w-[70%]">
+          <UserSearchBar />
+        </div>
+        
+        {/* this is sorting dropdowns */}
+        <div className="flex gap-3 me-5">
+          <UserSortDropDown />
+          <UserSortDropDown />
+        </div>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default UsersTable;
